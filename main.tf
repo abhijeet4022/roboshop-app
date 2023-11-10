1a248128bbd424a9bdf2c727d71539d5107d241f
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-10 02:30:47 +0000

    line alligement

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ module "vpc" {
   source = "git::https://github.com/abhijeet4022/tf-module-vpc.git"
 
   for_each = var.vpc
-  cidr = each.value["cidr"]
-  subnets = each.value["subnets"]
-  tags = each.key
+  cidr     = each.value["cidr"]
+  subnets  = each.value["subnets"]
+  tags     = each.key
 
 }
 
