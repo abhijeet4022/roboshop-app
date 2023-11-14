e50f6e439e0e8dfe7b4ce8c27d1d4fc88efba1af
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-14 19:45:23 +0530

    Terraform: Roboshop-App Terraform

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ module "vpc" {
   for_each                   = var.vpc
   cidr                       = each.value["cidr"]
   subnets                    = each.value["subnets"]
-  tags                       = each.key
   default_vpc_id             = var.default_vpc_id
   default_vpc_cidr           = var.default_vpc_cidr
   default_vpc_route_table_id = var.default_vpc_route_table_id
