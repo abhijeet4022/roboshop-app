e8da9b7f4fff071a84776f75152cc43a8b6adee7
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-15 10:44:47 +0530

    Terraform: Roboshop-App Terraform

--- main.tf
+++ main.tf
@@ -11,20 +11,20 @@ module "vpc" {
   tags                       = var.tags
 }
 
-
-module "alb" {
-  source = "git::https://github.com/abhijeet4022/tf-module-alb.git"
-
-  for_each        = var.alb
-  internal        = each.value["internal"]
-  lb_type         = each.value["lb_type"]
-  env             = var.env
-  tags            = var.tags
-  vpc_id          = each.value["internal"] ? local.vpc_id : var.default_vpc_id
-  sg_port         = each.value["sg_port"]
-  sg_ingress_cidr = each.value["sg_ingress_cidr"]
-  subnets         = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
-}
+#
+#module "alb" {
+#  source = "git::https://github.com/abhijeet4022/tf-module-alb.git"
+#
+#  for_each        = var.alb
+#  internal        = each.value["internal"]
+#  lb_type         = each.value["lb_type"]
+#  env             = var.env
+#  tags            = var.tags
+#  vpc_id          = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+#  sg_port         = each.value["sg_port"]
+#  sg_ingress_cidr = each.value["sg_ingress_cidr"]
+#  subnets         = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
+#}
 
 
 
--- output.tf
+++ output.tf
@@ -1,10 +1,15 @@
-output "vpc" {
-  value = module.vpc
+#output "vpc" {
+#  value = module.vpc
+#}
+#
+#output "subnets" {
+#  value = data.aws_subnets.subnets.ids
+#}
+
+output "local" {
+  value = local.app_subnets
 }
 
-output "subnets" {
-  value = data.aws_subnets.subnets.ids
-}
 
 
 #output "alb" {
