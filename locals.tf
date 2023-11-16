53c9ebed65369b6fd52fe5fa1fc4ff2bad96abd2
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-16 12:29:03 +0530

    Terraform: Roboshop-App Terraform

--- locals.tf
+++ locals.tf
@@ -2,8 +2,10 @@ locals {
   vpc_id      = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   app_subnets = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnets_ids", null): v.id]
   db_subnets = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "db", null), "subnets_ids", null): v.id]
+  app_subnets_cidr  = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "db", null), "subnets_ids", null): v.cidr_block]
 
-#  db_subnets = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "db", null), "subnets_ids", null) : v.id ]
-#
-#  app_subnets_cidr = [ for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnets_ids" null) : v.cidr_block ]
+
+  #  db_subnets = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "db", null), "subnets_ids", null) : v.id ]
+  #
+  #  app_subnets_cidr = [ for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnets_ids" null) : v.cidr_block ]
 }
--- main.tf
+++ main.tf
@@ -36,10 +36,18 @@ module "docdb" {
   env    = var.env
   tags   = var.tags
 
-  for_each = var.docdb
-  subnet_ids = local.db_subnets
+  for_each                = var.docdb
+  subnet_ids              = local.db_subnets
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  vpc_id                  = local.vpc_id
+  docdb_sg_ingress_cidr   = local.app_subnets_cidr
+  engine_version          = each.value["engine_version"]
 
 }
+
+
 
 
 
