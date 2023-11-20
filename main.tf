8c725411a503fe89e9c6e1e8f6e198baca6f52a3
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-20 08:25:49 +0530

    Terraform: Roboshop-App Terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 # Configure the VPC.
 module "vpc" {
+
   source                     = "git::https://github.com/abhijeet4022/tf-module-vpc.git"
   env                        = var.env
   tags                       = var.tags
@@ -13,8 +14,11 @@ module "vpc" {
 }
 
 
+
+
 # Configure the Application Load Balancer.
 module "alb" {
+
   source          = "git::https://github.com/abhijeet4022/tf-module-alb.git"
   env             = var.env
   tags            = var.tags
@@ -25,11 +29,14 @@ module "alb" {
   sg_port         = each.value["sg_port"]
   sg_ingress_cidr = each.value["sg_ingress_cidr"]
   subnets         = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
+
 }
+
 
 
 # Configure the DocumentDB/Mongodb.
 module "docdb" {
+
   source                  = "git::https://github.com/abhijeet4022/tf-module-docdb.git"
   env                     = var.env
   tags                    = var.tags
@@ -47,7 +54,10 @@ module "docdb" {
 
 }
 
+
+
 module "rds" {
+
   source                  = "git::https://github.com/abhijeet4022/tf-module-rds.git"
   env                     = var.env
   tags                    = var.tags
@@ -69,7 +79,10 @@ module "rds" {
 }
 
 
+
+
 module "elasticache" {
+
   source                 = "git::https://github.com/abhijeet4022/tf-module-elasticache.git"
   env                    = var.env
   tags                   = var.tags
@@ -87,7 +100,11 @@ module "elasticache" {
 
 }
 
+
+
+
 module "rabbitmq" {
+
   source              = "git::https://github.com/abhijeet4022/tf-module-rabbitmq.git"
   env                 = var.env
   tags                = var.tags
