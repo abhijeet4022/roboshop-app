50d66af595c2e2de4b3445489f30560517f7b7a5
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-25 10:42:09 +0530

    Terraform: Roboshop-App Terraform

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "vpc" {
-  value = module.vpc
-}
+#output "vpc" {
+#  value = module.vpc
+#}
 
 #output "subnets" {
 #  value = data.aws_subnets.subnets.ids
@@ -9,7 +9,7 @@ output "vpc" {
 #output "alb" {
 #  value = module.alb
 #}
-
-output "listener" {
-  value = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
-}
+#
+#output "listener" {
+#  value = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+#}
